e141e323d895c7df021354437f20c9afa1de422e
Maximo van der Raadt <mvanderr@edu.uwaterloo.ca>
2021-01-31 15:22:37 +0000

    go fast

---
+++ Assets/soundEffectController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class soundEffectController : MonoBehaviour
+{
+    public AudioSource audiosource;
+
+    public AudioClip bonk;
+    public AudioClip moveWoosh;
+    public AudioClip win;
+
+    void playBonk(){
+        audiosource.clip = bonk;
+    }
+
+    void playWoosh(){
+        audiosource.clip = moveWoosh;
+    }
+
+    void playWin(){
+        audiosource.clip = win;
+    }
+}
