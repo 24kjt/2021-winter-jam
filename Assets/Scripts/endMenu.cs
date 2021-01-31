d91bb405392d11082660c6cf318cbf6126d7cf7b
Maximo van der Raadt <mvanderr@edu.uwaterloo.ca>
2021-01-31 19:13:09 +0000

    some cutscenes

---
+++ Assets/Scripts/endMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class endMenu : MonoBehaviour
+{
+    int currentScreen = 0;
+
+    public GameObject endscene1;
+    public GameObject endscene2;
+
+    void Update(){
+        switch(currentScreen) {
+            case 0:
+                if(Input.anyKeyDown){
+                    currentScreen++;
+                    endscene1.SetActive(false);
+                    endscene2.SetActive(true);
+                }
+                break;
+            case 1:
+                if(Input.anyKeyDown){
+                    SceneManager.LoadScene(0);
+                }
+                break;
+            default:
+                break;
+        }
+    }
+}
--- Assets/Scripts/mainMenu.cs
+++ Assets/Scripts/mainMenu.cs
@@ -5,8 +5,52 @@ using UnityEngine.SceneManagement;
 
 public class mainMenu : MonoBehaviour
 {
+    int currentScreen = 0;
+    bool keyDown = false;
+
+    public GameObject cutscene1;
+    public GameObject cutscene2;
+    public GameObject instructions2;
+
+    void Update(){
+        switch(currentScreen) {
+            case 1:
+                if(Input.anyKeyDown){
+                    currentScreen++;
+                    cutscene1.SetActive(false);
+                    cutscene2.SetActive(true);
+                    keyDown = true;
+                }
+                break;
+            case 2:
+                if(Input.anyKeyDown){
+                    currentScreen++;
+                    cutscene2.SetActive(false);
+                    instructions2.SetActive(true);
+                    keyDown = true;
+                }
+                break;
+            case 3:
+                if (Input.anyKeyDown && !keyDown){
+                    PlayGame();
+                } 
+                if (keyDown && !Input.anyKeyDown) {
+                    keyDown = false;
+                }
+                break;
+            default:
+                break;
+        }
+    }
     public void PlayGame() {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void PlayButton(){
+        currentScreen++;
+        GameObject.Find("Canvas").SetActive(false);
+        GameObject.Find("title page").SetActive(false);
+        cutscene1.SetActive(true);
     }
 
     public void QuitGame() {
