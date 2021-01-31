eda28014445de8877c6a7f34e2b24bdcc2577941
Maximo van der Raadt <mvanderr@edu.uwaterloo.ca>
2021-01-31 04:48:16 +0000

    many things

---
+++ Assets/Scripts/musicManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class musicManager : MonoBehaviour
+{
+    public AudioSource audioSource;
+     private void Awake()
+     {
+         DontDestroyOnLoad(transform.gameObject);
+     }
+ 
+     public void PlayMusic()
+     {
+         if (audioSource.isPlaying) 
+            return;
+         audioSource.Play();
+     }
+ 
+     public void StopMusic()
+     {
+         audioSource.Stop();
+     }
+}
